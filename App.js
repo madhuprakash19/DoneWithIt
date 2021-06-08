17d95c519113eb0b9748fa2f545643b06d7a6636
madhuprakash19 <madthtp11@gmail.com>
2021-06-08 14:48:22 +0530

    Added custom text component

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, Image,TouchableOpacity, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, Image,TouchableOpacity, SafeAreaView, View} from 'react-native';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
+import AppText from './app/components/AppText'
+
 export default function App() {
-  return <ViewImageScreen/>;
+  return (
+    <View
+      style={{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",  
+      }}
+    >
+      <AppText>I love React Native!</AppText>
+    </View>
+  );
 }
 
