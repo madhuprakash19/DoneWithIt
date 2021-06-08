2cf33d29f0cdb4ca2db5cea55290c5593db1b517
madhuprakash19 <madthtp11@gmail.com>
2021-06-08 20:10:56 +0530

    created reusable card component

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, Image,TouchableOpacity, SafeAreaView, View} from 'react-native';
-
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import Card from './app/components/Card'
 
 export default function App() {
   return (
-   <WelcomeScreen />
+   <View
+   style = {{
+     backgroundColor : '#f8f4f4',
+     padding: 20,
+     paddingTop : 70,
+   }}
+   >
+     <Card 
+     title = "Red jacket for sale"
+     subTitle = "$100"
+     image={require("./app/assets/jacket.jpg")}
+     />
+   </View>
   );
 }
 
