2f12f83796c22361c48b8663352c9c614271684c
madhuprakash19 <madthtp11@gmail.com>
2021-06-08 16:06:26 +0530

    Added custom button and colors for button

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, Image,TouchableOpacity, SafeAreaView, View} from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
 
-import AppText from './app/components/AppText'
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center",  
-      }}
-    >
-      <AppText>I love React Native!</AppText>
-    </View>
+   <WelcomeScreen />
   );
 }
 
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Image, ImageBackground ,StyleSheet,View,Text} from 'react-native';
+import AppButton from '../components/AppButton';
+import colors from '../config/colors';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
+        blurRadius={5}
         source={require("../assets/background.jpg")}
         style={styles.background}
         >
             <View style={styles.logoContainer}>
             <Image source={require('../assets/logo-red.png')} style={styles.logo}/>
-            <Text>Sell What You Don't Need</Text>
+            <Text style={styles.tagline}>Sell What You Don't Need</Text>
             </View>
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonsContainer}>
+            <AppButton title={"Login"}/>
+            <AppButton title={"Register"} color={"secondary"}/>
+            </View>
         </ImageBackground>
             
     );
@@ -24,15 +29,9 @@ const styles = StyleSheet.create({
         justifyContent:"flex-end",
         alignItems:"center",
     },
-    loginButton : {
-        width: '100%',
-        height : 70,
-        backgroundColor : '#fc5c65',
-    },
-    registerButton : {
-        width: '100%',
-        height : 70,
-        backgroundColor : '#4ecdc4',
+    buttonsContainer:{
+        padding:20,
+        width:'100%'
     },
     logo : {
         width :100,
@@ -44,6 +43,12 @@ const styles = StyleSheet.create({
         position : 'absolute',
         top : 120, 
         alignItems : "center",
+    },
+    tagline : {
+        fontSize : 20,
+        fontWeight : "300",
+        paddingVertical : 30,
+
     }
 })
 
