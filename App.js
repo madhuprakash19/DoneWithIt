8837388ae7714325ceb936d3fcaf92657844fd2b
madhuprakash19 <madthtp11@gmail.com>
2021-06-09 11:42:54 +0530

    created list item card and improved image screen

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, Image,TouchableOpacity, SafeAreaView, View} from 'react-native';
-import Card from './app/components/Card'
+import ViewImageScreen from './app/screens/ViewImageScreen';
+
 
 export default function App() {
   return (
-   <View
-   style = {{
-     backgroundColor : '#f8f4f4',
-     padding: 20,
-     paddingTop : 70,
-   }}
-   >
-     <Card 
-     title = "Red jacket for sale"
-     subTitle = "$100"
-     image={require("./app/assets/jacket.jpg")}
-     />
-   </View>
-  );
+    <ViewImageScreen />
+    );
 }
 
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -12,4 +12,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default AppText;
+export default AppText; 
